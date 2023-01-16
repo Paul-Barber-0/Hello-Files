23b16bc648a88479a4e01e119ef215030a6e2977
Paul-Barber-0 <paulbarber16@gmail.com>
2023-01-15 19:18:08 -0500

    Added public folder

--- server.js
+++ server.js
@@ -2,4 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static('public'));
+
+// app.use('/', (req, res) => {
+//     res.sendFile(__dirname + "/index.html");
+// });
+
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
